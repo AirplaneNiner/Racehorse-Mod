6e7c02a18f8937bf6581e2577dea59cd1c6d3800
AirplaneNiner <91838121+AirplaneNiner@users.noreply.github.com>
2026-01-23 10:32:08 +0200

    Fix overlay speed not matching ingame speed

--- src/main/java/net/airplaneniner/horsesprint/procedures/SprintBarDisplaySpeedProcedure.java
+++ src/main/java/net/airplaneniner/horsesprint/procedures/SprintBarDisplaySpeedProcedure.java
@@ -4,11 +4,13 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 
+import java.util.Objects;
+
 public class SprintBarDisplaySpeedProcedure {
 	public static String execute(Entity entity) {
 		if (entity == null)
 			return "";
 		return "Speed: " + (new java.text.DecimalFormat("##.##")
-				.format(((entity.getVehicle()) instanceof LivingEntity _livingEntity1 && _livingEntity1.getAttributes().hasAttribute(Attributes.MOVEMENT_SPEED) ? _livingEntity1.getAttribute(Attributes.MOVEMENT_SPEED).getValue() : 0) * 43.17));
+				.format((((entity.getVehicle()) instanceof LivingEntity _livingEntity1 && _livingEntity1.getAttributes().hasAttribute(Attributes.MOVEMENT_SPEED) ? Objects.requireNonNull(_livingEntity1.getAttribute(Attributes.MOVEMENT_SPEED)).getValue() : 0) * 43.17) - 0.29));
 	}
 }
