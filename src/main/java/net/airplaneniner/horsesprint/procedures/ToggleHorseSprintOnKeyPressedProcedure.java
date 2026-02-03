981fd382e8e953e00ab8c7936f22f397d94e79ad
AirplaneNiner <91838121+AirplaneNiner@users.noreply.github.com>
2026-02-03 19:26:18 +0200

    Add gelding speed changes

--- src/main/java/net/airplaneniner/horsesprint/procedures/ToggleHorseSprintOnKeyPressedProcedure.java
+++ src/main/java/net/airplaneniner/horsesprint/procedures/ToggleHorseSprintOnKeyPressedProcedure.java
@@ -1,5 +1,7 @@
 package net.airplaneniner.horsesprint.procedures;
 
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import sekelsta.horse_colors.entity.HorseGeneticEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -12,24 +14,37 @@ import net.airplaneniner.horsesprint.init.HorseSprintModAttributes;
 import java.util.Objects;
 
 public class ToggleHorseSprintOnKeyPressedProcedure {
-	public static void execute(Entity entity) {
-		if (entity == null)
-			return;
-		if ((entity.getVehicle()) instanceof HorseGeneticEntity) {
+    public static void execute(Entity entity) {
+        if (entity == null)
+            return;
+        if ((entity.getVehicle()) instanceof HorseGeneticEntity) {
             if (entity.getCapability(HorseSprintModVariables.PLAYER_VARIABLES).orElseGet(HorseSprintModVariables.PlayerVariables::new).horseCanSprint
                     && ((entity.getVehicle()) instanceof LivingEntity _livingEntity3 && _livingEntity3.getAttributes().hasAttribute(HorseSprintModAttributes.HORSE_SPURT_COOLDOWN.get())
                     ? Objects.requireNonNull(_livingEntity3.getAttribute(HorseSprintModAttributes.HORSE_SPURT_COOLDOWN.get())).getBaseValue()
                     : 0) == 0) {
-                if ((entity.getVehicle()) instanceof LivingEntity _entity) {
-                    AttributeModifier modifier = new AttributeModifier("horse_sprint:spurt",
-                            ((entity.getVehicle()) instanceof LivingEntity _livingEntity5 && _livingEntity5.getAttributes().hasAttribute(HorseSprintModAttributes.HORSE_STAMINA.get())
-                                    ? Objects.requireNonNull(_livingEntity5.getAttribute(HorseSprintModAttributes.HORSE_STAMINA.get())).getBaseValue()
-                                    : 0),
-                            AttributeModifier.Operation.ADDITION);
-                    if (Objects.requireNonNull(_entity.getAttribute(Attributes.MOVEMENT_SPEED)).getModifiers().stream().noneMatch((e) -> e.getName().equals(modifier.getName()))) {
-                        Objects.requireNonNull(_entity.getAttribute(Attributes.MOVEMENT_SPEED)).addPermanentModifier(modifier);
+                    if (!((HorseGeneticEntity) entity.getVehicle()).isFertile()) {
+                        if ((entity.getVehicle()) instanceof LivingEntity _entity) {
+                            AttributeModifier modifier = new AttributeModifier("horse_sprint:spurt",
+                                    (((entity.getVehicle()) instanceof LivingEntity _livingEntity6 && _livingEntity6.getAttributes().hasAttribute(HorseSprintModAttributes.HORSE_STAMINA.get())
+                                            ? Objects.requireNonNull(_livingEntity6.getAttribute(HorseSprintModAttributes.HORSE_STAMINA.get())).getBaseValue()
+                                            : 0) + Mth.nextDouble(RandomSource.create(), 0.015, 0.04)),
+                                    AttributeModifier.Operation.ADDITION);
+                            if (Objects.requireNonNull(_entity.getAttribute(Attributes.MOVEMENT_SPEED)).getModifiers().stream().noneMatch((e) -> e.getName().equals(modifier.getName()))) {
+                                Objects.requireNonNull(_entity.getAttribute(Attributes.MOVEMENT_SPEED)).addPermanentModifier(modifier);
+                            }
+                        }
+                    } else {
+                        if ((entity.getVehicle()) instanceof LivingEntity _entity) {
+                            AttributeModifier modifier = new AttributeModifier("horse_sprint:spurt",
+                                    ((entity.getVehicle()) instanceof LivingEntity _livingEntity11 && _livingEntity11.getAttributes().hasAttribute(HorseSprintModAttributes.HORSE_STAMINA.get())
+                                            ? Objects.requireNonNull(_livingEntity11.getAttribute(HorseSprintModAttributes.HORSE_STAMINA.get())).getBaseValue()
+                                            : 0),
+                                    AttributeModifier.Operation.ADDITION);
+                            if (Objects.requireNonNull(_entity.getAttribute(Attributes.MOVEMENT_SPEED)).getModifiers().stream().noneMatch((e) -> e.getName().equals(modifier.getName()))) {
+                                Objects.requireNonNull(_entity.getAttribute(Attributes.MOVEMENT_SPEED)).addPermanentModifier(modifier);
+                            }
+                        }
                     }
-                }
                 {
                     entity.getCapability(HorseSprintModVariables.PLAYER_VARIABLES).ifPresent(capability -> {
                         capability.horseIsSprinting = true;
@@ -38,7 +53,7 @@ public class ToggleHorseSprintOnKeyPressedProcedure {
                     });
                 }
                 TickSpurtTimerProcedure.execute(entity);
-			}
-		}
-	}
+            }
+        }
+    }
 }
